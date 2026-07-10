ac12daf8d347a4d04c7e2a2393d87aaeb50ca1ca
Sabrina <sabrinarach.sr@gmail.com>
2026-07-10 11:52:02 +0200

    deleted onSubmit and onClick from the function in the NavPagination.js file as it is obsolete

--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -1,9 +1,8 @@
-export function NavPagination (onSubmit, onClick) {
-const pag = document.createElement("span");
-pag.classList.add("navigation__pagination");
-pag.dataset.js = "pagination";
-pag.textContent = "1 / 42";
+export function NavPagination() {
+  const pag = document.createElement("span");
+  pag.classList.add("navigation__pagination");
+  pag.dataset.js = "pagination";
+  pag.textContent = "1 / 42";
 
-return pag;
+  return pag;
 }
-
