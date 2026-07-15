ea189e167049229fd14d46e4bd71c94592a96203
Sabrina <sabrinarach.sr@gmail.com>
2026-07-15 10:28:17 +0200

    added Code to scroll to top when clicking next or prev button

--- components/NavButton/NavButton.js
+++ components/NavButton/NavButton.js
@@ -1,32 +1,31 @@
-
 // onClickPrev und onClickNext sind callback functions
-export function NavButton (onClickPrev, onClickNext) {
+export function NavButton(onClickPrev, onClickNext) {
   const nav = document.createElement("nav");
   nav.classList.add("navigation");
   nav.dataset.js = "navigation";
 
-    const prevButton = document.createElement("button");
+  const prevButton = document.createElement("button");
   prevButton.classList.add("button", "button--prev");
   prevButton.dataset.js = "button-prev";
   prevButton.textContent = "previous";
 
-    const nextButton = document.createElement("button");
+  const nextButton = document.createElement("button");
   nextButton.classList.add("button", "button--next");
   nextButton.dataset.js = "button-next";
   nextButton.textContent = "next";
 
-
-// Klick-Events
+  // Klick-Events
   prevButton.addEventListener("click", () => {
     prevButton.classList.add("button-click");
     onClickPrev();
+    window.scrollTo({ top: 0, behavior: "smooth" }); //scrollt bei Klick auf Button wieder zum Anfang der vorherigen Seite
   });
 
   nextButton.addEventListener("click", () => {
     nextButton.classList.add("button-click");
     onClickNext();
+    window.scrollTo({ top: 0, behavior: "smooth" });
   });
-
 
   // Animation nach Ende entfernen
   // wenn Button "animationed" ist, dann entferne die Klasse. Beim erneuten Klick startet die Animation wieder neu
@@ -39,9 +38,7 @@ export function NavButton (onClickPrev, onClickNext) {
     nextButton.classList.remove("button-click");
   });
 
-nav.append(prevButton, nextButton);
+  nav.append(prevButton, nextButton);
 
-return nav;
-
+  return nav;
 }
-
